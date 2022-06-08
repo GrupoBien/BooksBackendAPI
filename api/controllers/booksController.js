d1ee3a2b91a14f1d6bd41ec07030116a01af27dd
Darwin_N19 <82281869+suzubki@users.noreply.github.com>
2022-06-07 21:03:44 -0500

    feat: controller updateBook added

--- api/controllers/booksController.js
+++ api/controllers/booksController.js
@@ -1,3 +1,5 @@
+// import from ...
+
 export const updateBook = async (req, res) => {
     const { id } = req.body;
     // const regex = /^[a-z][0-9]*$/ etc etc
@@ -19,4 +21,23 @@ export const updateBook = async (req, res) => {
     return res.status(200).json({
         message: "Libros actualizados correctamente",
     });
+};
+
+export const deleteBook = async (req, res) => {
+    const { id } = req.body;
+    // const regex = /^[a-z][0-9]*$/ etc etc
+    // if (!regex.matches(id)) res.status(400).json({message: "Id inválido"})
+
+    try {
+        // si modelo se llama ModeloBooks -->
+        const book = await ModeloBooks.findByIdAndDelete(id);
+        res.status(201).json({
+            message: "Libro eliminado",
+            data: book,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: "Error al eliminar libro",
+        });
+    }
 };
