fa378ae1e03aa53e932779501b47c48756cd8c9a
Darwin_N19 <82281869+suzubki@users.noreply.github.com>
2022-06-07 21:01:33 -0500

    feat: updateBook controller

---
+++ api/controllers/booksController.js
@@ -0,0 +1,22 @@
+export const updateBook = async (req, res) => {
+    const { id } = req.body;
+    // const regex = /^[a-z][0-9]*$/ etc etc
+    // if (!regex.matches(id)) res.status(400).json({message: "Id inválido"})
+
+    try {
+        // si modelo se llama ModeloBooks -->
+        const book = await ModeloBooks.findByIdAndUpdate(id, req.body);
+        res.status(201).json({
+            message: "Libro actualizado",
+            data: book,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: "Error al actualizar libro",
+        });
+    }
+
+    return res.status(200).json({
+        message: "Libros actualizados correctamente",
+    });
+};
