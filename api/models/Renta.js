ca2d855a3013ada38e58b476ca0c03a034b3d7da
angel gamaliel <angel.gamaliel@hotmail.com>
2022-06-08 20:49:19 -0500

    set up enviroment

--- api/models/Renta.js
+++ api/models/Renta.js
@@ -4,7 +4,7 @@ const rentaSchema= new Schema({
     quantity:Number,
     startRentDate:{type: Date,default:Date.now},
     endRentDate:Date,
-    books:[{type:Schema.Types.ObjectId,ref:"Libro"}],
-    cliente:{type:Schema.type.ObjectId,ref:"Cliente"}
+    books:[{type:Schema.Types.ObjectId,ref:"Book"}],
+    cliente:{type:Schema.type.ObjectId,ref:"cliente"}
 })
 export default model("Renta",rentaSchema)
