53b842fab5ec9388d86afc5e01d7295f7c491101
Darwin_N19 <82281869+suzubki@users.noreply.github.com>
2022-06-08 21:17:31 -0500

    feat: routes and controller for rental history, just added a getAllRentals method

---
+++ api/controllers/rentalController.js
@@ -0,0 +1,18 @@
+import rentalSchema from "../models/renta.js";
+
+const getAllRentals = (req, res) => {
+    try {
+        const rentals = rentalSchema.find();
+
+        return res.status(200).json({
+            message: "Ok",
+            rentals,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: "Error al obtener todas las rentas",
+        });
+    }
+};
+
+export { getAllRentals };
--- api/routes/booksRoute.js
+++ api/routes/booksRoute.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { booksController } from "../controllers/index.js";
 
-export const router = Router();
+const router = Router();
 
 router.put("/books/:id", booksController.updateBook);
 router.delete("/books/:id", booksController.deleteBook);
+
+export { router };
