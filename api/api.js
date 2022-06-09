f9cd8261a4b4eaf1a5115286544bfa76a1e2f812
servo pcerda <fernandonando98@live.com.mx>
2022-06-08 21:44:15 -0500

    feat: regresando los comentarios

--- api/api.js
+++ api/api.js
@@ -3,6 +3,14 @@ import { booksRoute } from './routes/index.js';
 
 const api = express();
 
+/**
+ * Configuro primeros middlewares
+ *
+ * Configurar rutas
+ *
+ * Configurar Generic ErrorHandler
+ */
+
 api.use('/', booksRoute);
 
 export default api;
