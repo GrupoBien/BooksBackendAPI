179c621bb0f964cdc9d803a375e541cf3c9fa51c
Raul Sierra <j.raulsierra.uni@gmail.com>
2022-06-09 17:56:07 -0600

    fix: change user to email & deleted the login model because were are going to use the user model

--- api/models/login.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const LoginSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},//we set required our inputs, username has to been unique.
-    password: {type: String, required: true},
-  });
-
-  
-export default mongoose.model('Login', LoginSchema);
--- api/validators/loginValidator.js
+++ api/validators/loginValidator.js
@@ -1,12 +1,8 @@
 import Joi from 'joi';
 
 export const loginValidator = Joi.object({
-    username: Joi.string()
-        .alphanum()
-        .min(3)
-        .max(30)
-        .required(),
-
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')
         .required()
