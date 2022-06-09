0a936b5ce759e87f4a8aa762f8cb12c60d0564fc
Raul Sierra <j.raulsierra.uni@gmail.com>
2022-06-08 19:48:27 -0600

    feat: login controller created with decrypted and a access token

--- api/models/login.js
+++ api/models/login.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const LoginSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},//we set required our inputs, username has to been unique.
     password: {type: String, required: true},
   });
 
   
-export default mongoose.model('User', UserSchema);
+export default mongoose.model('Login', LoginSchema);
