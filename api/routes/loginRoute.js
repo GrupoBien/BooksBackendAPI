e5d42e816210255494b5f4f72844a61caa55093d
Raul Sierra <j.raulsierra.uni@gmail.com>
2022-06-09 18:33:05 -0600

    fix: export in router login

--- api/routes/loginRoute.js
+++ api/routes/loginRoute.js
@@ -6,4 +6,6 @@ export const router = Router();
 
 /* route who get the username and the password and send it to userController
     we use the middleware isLoggedIn to validate the username and password was get it*/
-router.get('/login', isLoggedIn, login.getLogin);
+routerLogin.get('/login', isLoggedIn, login.getLogin);
+
+export {routerLogin};
