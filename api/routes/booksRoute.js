c9b501cc7216ff61976f9aa841a8a08e025a87e8
Miguel Ángel García Martínez <ma.garmar98@gmail.com>
2022-06-09 19:46:27 -0500

    feat: merging all books routes

--- api/routes/booksRoute.js
+++ api/routes/booksRoute.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { booksController } from "../controllers/index.js";
+import {validateBookeCreation} from '../middlewares/index.js'
 
 const router = Router();
 
+router.post('/books', validateBookeCreation, booksController.createBook)
 router.put("/books/:id", booksController.updateBook);
 router.delete("/books/:id", booksController.deleteBook);
 
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,3 +1,2 @@
-export { booksCreateRouter } from "./booksCreateRouter.js";
 export { router as booksRoute } from "./booksRoute.js";
 export { router as rentalRoute } from "./rentalRoute.js";
