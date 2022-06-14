77b805c9ffc876868ea5c4c7b21095faebbf764c
servo pcerda <fernandonando98@live.com.mx>
2022-06-14 18:59:20 -0500

    fix typo

--- api/validators/rentas.js
+++ api/validators/rentas.js
@@ -1,9 +1,9 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const rentar = Joi.object({
-    quantity: Joi.number().required(),
-    startRentDate:Joi.date,
-    endRentDate:Joi.date.required(),
-    books:Joi.array().items(String).required(),
-    cliente:Joi.string().required()
+  quantity: Joi.number().required(),
+  startRentDate: Joi.date(),
+  endRentDate: Joi.date().required(),
+  books: Joi.array().items(String).required(),
+  cliente: Joi.string().required(),
 });
