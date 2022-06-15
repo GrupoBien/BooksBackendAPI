153527f38518d6e221c4f4f56d757ea1fcbba499
servo pcerda <fernandonando98@live.com.mx>
2022-06-14 21:27:39 -0500

    register user one commit

--- api/validators/index.js
+++ api/validators/index.js
@@ -1,7 +1,14 @@
 import booksCreateValidator from './booksCreateValidator.js';
 //TODO: fix import
 import { updateBooks } from './books.js';
-import { rentar } from './rentas.js'
+import { rentar } from './rentas.js';
 import { loginValidator } from './loginValidator.js';
+import { validUserSchema } from './userValidator.js';
 
-export { booksCreateValidator, updateBooks,rentar, loginValidator };
+export {
+  booksCreateValidator,
+  updateBooks,
+  rentar,
+  loginValidator,
+  validUserSchema,
+};
