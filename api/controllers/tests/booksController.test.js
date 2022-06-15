1d84211e09b5c02e210e3bd4f4ede312a1db11a7
servo pcerda <fernandonando98@live.com.mx>
2022-06-15 18:08:07 -0500

    fix: add done

--- api/controllers/tests/booksController.test.js
+++ api/controllers/tests/booksController.test.js
@@ -14,6 +14,7 @@ afterEach(async () => {
 
 afterAll(async () => {
   await db.close();
+  done();
 });
 
 describe('Books Controller', () => {
--- api/controllers/tests/clientController.test.js
+++ api/controllers/tests/clientController.test.js
@@ -1,29 +1,30 @@
-import request from "supertest";
-import api from "../../api.js";
-import db from "../../config/databaseTest.js";
+import request from 'supertest';
+import api from '../../api.js';
+import db from '../../config/databaseTest.js';
 
 const agent = request.agent(api);
 
 beforeAll(async () => {
-    await db.connect();
+  await db.connect();
 });
 
 afterEach(async () => {
-    await db.clear();
+  await db.clear();
 });
 
 afterAll(async () => {
-    await db.close();
+  await db.close();
+  done();
 });
 
-describe("Client Controller", () => {
-    test("Delete Client --> id error", async () => {
-        const response = agent.del("/clients/62a93bd339362736992ad953");
-        const urlExists = response.url.includes(
-            "/clients/62a93bd339362736992ad953"
-        );
+describe('Client Controller', () => {
+  test('Delete Client --> id error', async () => {
+    const response = agent.del('/clients/62a93bd339362736992ad953');
+    const urlExists = response.url.includes(
+      '/clients/62a93bd339362736992ad953'
+    );
 
-        expect(response.method).toBe("DELETE");
-        expect(urlExists).toBeTruthy();
-    });
+    expect(response.method).toBe('DELETE');
+    expect(urlExists).toBeTruthy();
+  });
 });
