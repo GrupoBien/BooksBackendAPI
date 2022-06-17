d23a6fb9ddd1a32993e30cbbf99204a3d081ed4b
Servín <fernandonando98@live.com.mx>
2022-06-16 21:41:25 -0500

    Update rentalController.js

--- api/controllers/rentalController.js
+++ api/controllers/rentalController.js
@@ -17,7 +17,7 @@ const createRenta=async(req,res)=>{
     }
     console.log(BooksNotExist,BooksExist)
     if(BooksNotExist.length>0)return res.status(404).json({msg:"Algunos libros no existen favor de verificalo"})
-    let currentRents=await rentalSchema.find({endRentDate:{$lte:new Date()}});
+    let currentRents=await rentalSchema.find({endRentDate:{$gte:new Date()}});
     if(currentRents.length>=2)return res.status(400).json({msg:"No puede tener mas de dos rentas activas"})
     const insertRent={quantity,endRentDate,books:BooksExist,cliente}
     const renta=await rentalSchema.create(insertRent)
